5ceb4885f76f117170d78ace5535972f96a43240
홍태민 <hongtaemin919@gmail.com>
2025-06-07 10:05:37 +0900

    initial commit

---
+++ src/rhythmtest2/main1.java
@@ -0,0 +1,14 @@
+package rhythmtest2;
+
+public class main1 {
+	
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+
+	public static void main(String[] args) {
+		
+		new DynamicBeat();
+
+	}
+
+}
