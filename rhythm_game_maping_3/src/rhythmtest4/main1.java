73079b795cb4a4d22b50fc9403e1520ac1bee52d
홍태민 <hongtaemin919@gmail.com>
2025-06-08 10:17:35 +0900

    rhythm_game_maping(3) 폴더 전체 업로드

---
+++ rhythm_game_maping_3/src/rhythmtest4/main1.java
@@ -0,0 +1,14 @@
+package rhythmtest4;
+
+public class main1 {
+	
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+
+	public static void main(String[] args) {
+		
+		new DynamicBeat();
+
+	}
+
+}
